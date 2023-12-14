c67a8d6ecf727db9e922094f7ecbfb93cb211ca3
skdbsqls <skdbsqls98@naver.com>
2023-12-14 12:55:58 +0900

    feat: 입력, 출력, 에러 메시지 상수화

---
+++ src/constants/message.js
@@ -0,0 +1,21 @@
+const INPUT_MESSAGE = Object.freeze({
+  length: "다리의 길이를 입력해주세요.\n",
+  move: "\n이동할 칸을 선택해주세요. (위: U, 아래: D)\n",
+  restart: "\n게임을 다시 시도할지 여부를 입력해주세요. (재시도: R, 종료: Q)\n",
+});
+
+const OUTPUT_MESSAGE = Object.freeze({
+  start: "다리 건너기 게임을 시작합니다.\n",
+  end: "\n최종 게임 결과",
+  result: (result) => `\n게임 성공 여부: ${result}`,
+  try: (n) => `총 시도한 횟수: ${n}`,
+});
+
+const ERROR_MESSAGE = Object.freeze({
+  onlyNumber: "[ERROR] 다리 길이는 숫자로 입력해 주세요.",
+  invalidLength: "[ERROR] 다리 길이는 3부터 20 사이의 숫자여야 합니다.",
+  invalidMove: "[ERROR] 이동할 칸은 U와 D 중 하나의 문자로 입력해 주세요.",
+  invalidRestart: "[ERROR] 재시작 여부는 R과 Q 중 하나의 문자로 입력해 주세요.",
+});
+
+export { INPUT_MESSAGE, OUTPUT_MESSAGE, ERROR_MESSAGE };
